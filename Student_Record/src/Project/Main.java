2cb20b3ae850d3d982ffcac19cea0613869a22f4
Robinsh Raj <123360793+Robinsh-Raj@users.noreply.github.com>
2024-03-02 23:28:13 +0530

    Delete Student_Record directory

--- Student_Record/src/Project/Main.java
+++
@@ -1,9 +0,0 @@
-package Project;
-import java.io.IOException;
-public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		StudentConsole sc= new StudentConsole();
-		sc.start();
-	}
-}
